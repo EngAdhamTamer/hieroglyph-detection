7853162447bb9fb0e6ae3b4aa069d88056d50869
Adham Tamer <147272422+EngAdhamTamer@users.noreply.github.com>
2025-12-17 23:55:23 +0200

    Update train_yolo.py with flexible paths

--- train_yolo.py
+++ train_yolo.py
@@ -1,18 +1,23 @@
 from ultralytics import YOLO
+import os
 
-if __name__ == "__main__":
-    # Load segmentation model
-    model = YOLO("yolov8n-seg.pt")
+   if __name__ == "__main__":
+       # Load segmentation model
+       model = YOLO("yolov8n-seg.pt")
+       
+       # Get the base directory
+       BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+       DATA_PATH = os.path.join(BASE_DIR, "dataset", "data.yaml")
 
-    model.train(
-        data="C:/Users/Compumarts/Desktop/final_cv_project/dataset/data.yaml",
-        task="segment",
-        epochs=80,
-        imgsz=640,
-        batch=8,
-        device=0,        # GPU | "cpu"
-        workers=4,
-        project="runs/segment",
-        name="hieroglyph_seg",
-        save=True
-    )
+       model.train(
+           data=DATA_PATH,
+           task="segment",
+           epochs=80,
+           imgsz=640,
+           batch=8,
+           device=0,        # GPU | "cpu"
+           workers=4,
+           project="runs/segment",
+           name="hieroglyph_seg",
+           save=True
+       )
